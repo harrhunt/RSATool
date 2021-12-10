5efd6fe89740b6c95bb23364577a703144ca7948
harrhunt <harrhunt@isu.edu>
2021-12-09 22:02:03 -0700

    removed content from main block

--- RSA/crypto.py
+++ RSA/crypto.py
@@ -1,6 +1,5 @@
+from RSA.keys import PublicKey, PrivateKey
 import os
-
-from RSA.keys import PublicKey, PrivateKey, load_keys
 
 
 def bytes_to_int(data):
@@ -28,7 +27,6 @@ def encrypt(string, pub_key: PublicKey):
     blocksize = pub_key.byte_length - 11
     div_mod_res = divmod(string_length, blocksize)
     result = []
-    print(div_mod_res[0]+1)
     for block in range(div_mod_res[0] if div_mod_res[1] == 0 else div_mod_res[0] + 1):
         result.append(int_to_bytes(pub_key.encrypt(bytes_to_int(pad(string[block * blocksize:(block + 1) * blocksize], key_size))), key_size))
     return b"".join(result)
@@ -37,7 +35,7 @@ def encrypt(string, pub_key: PublicKey):
 def decrypt(string, priv_key: PrivateKey):
     string_length = len(string)
     key_size = priv_key.byte_length
-    blocksize = priv_key.byte_length - 11
+    # blocksize = priv_key.byte_length - 11
     div_mod_res = divmod(string_length, key_size)
     result = []
     for block in range(div_mod_res[0] if div_mod_res[1] == 0 else div_mod_res[0] + 1):
@@ -67,41 +65,3 @@ def decrypt_file(filepath, priv_key: PrivateKey):
     decrypted = decrypt(data, priv_key)
     with open(os.path.join(path, filename + ".DEC"), "wb") as file:
         file.write(decrypted)
-
-
-if __name__ == '__main__':
-    pvk, pbk = load_keys("1024")
-    encrypt_file("C:/Users/RockP/Downloads/Exercise1_BufferOverflow_Task2.mp4", pbk)
-    decrypt_file("C:/Users/RockP/Downloads/Exercise1_BufferOverflow_Task2.mp4", pvk)
-    # encrypted = encrypt(
-    #     "This is a really long test message to test all the functions that I want to test This is a really long test "
-    #     "message to test all the functions that I want to test This is a really long test message to test all the "
-    #     "functions that I want to test This is a really long test message to test all the functions that I want to "
-    #     "test This is a really long test message to test all the functions that I want to test This is a really long "
-    #     "test message to test all the functions that I want to test This is a really long test message to test all "
-    #     "the functions that I want to test This is a really long test message to test all the functions that I want "
-    #     "to test This is a really long test message to test all the functions that I want to test This is a really "
-    #     "long test message to test all the functions that I want to test This is a really long test message to test "
-    #     "all the functions that I want to test This is a really long test message to test all the functions that I "
-    #     "want to test This is a really long test message to test all the functions that I want to test This is a "
-    #     "really long test message to test all the functions that I want to test This is a really long test message to "
-    #     "test all the functions that I want to test This is a really long test message to test all the functions that "
-    #     "I want to test This is a really long test message to test all the functions that I want to test This is a "
-    #     "really long test message to test all the functions that I want to test This is a really long test "
-    #     "message to test all the functions that I want to test This is a really long test message to test all the "
-    #     "functions that I want to test This is a really long test message to test all the functions that I want to "
-    #     "test This is a really long test message to test all the functions that I want to test This is a really long "
-    #     "test message to test all the functions that I want to test This is a really long test message to test all "
-    #     "the functions that I want to test This is a really long test message to test all the functions that I want "
-    #     "to test This is a really long test message to test all the functions that I want to test This is a really "
-    #     "long test message to test all the functions that I want to test This is a really long test message to test "
-    #     "all the functions that I want to test This is a really long test message to test all the functions that I "
-    #     "want to test This is a really long test message to test all the functions that I want to test This is a "
-    #     "really long test message to test all the functions that I want to test This is a really long test message to "
-    #     "test all the functions that I want to test This is a really long test message to test all the functions that "
-    #     "I want to test This is a really long test message to test all the functions that I want to test".encode(
-    #         "utf-8"),
-    #     pbk)
-    # print(encrypted)
-    # decrypted = decrypt(encrypted, pvk)
-    # print(decrypted)
